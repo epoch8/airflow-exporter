b8ab63e10f05a19cb5b591c0dda92f2ca6d70f17
Andrey Tatarinov <elephantropy@gmail.com>
2018-09-22 12:21:43 +0300

    use common dag definition for all test envs

---
+++ tests/dags/dummy_dag.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators import DummyOperator
+
+
+default_args = {
+    'owner': 'owner',
+    'depends_on_past': False,
+    'start_date': datetime.now(),
+    'provide_context': True,
+    'retries': 100,
+    'retry_delay': timedelta(seconds=30),
+    'max_active_runs': 1,
+    'schedule_interval': timedelta(seconds=5),
+}
+
+
+dag = DAG('dummy_dag', default_args=default_args)
+
+dummy1 = DummyOperator(
+    task_id='dummy_task_1',
+    dag=dag
+)
+
+dummy2 = DummyOperator(
+    task_id='dummy_task_2',
+    dag=dag
+)
+
+dummy1 >> dummy2
