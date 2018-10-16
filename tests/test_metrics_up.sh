f6bdaa787cfa2d2a4d7661d88f66314ea45dc30f
Cameron Yick <cameron.yick@yale.edu>
2018-10-16 01:34:55 -0400

    Relax Permissions for Python3
    Run as root user due to changes in python logging

--- tests/test_metrics_up.sh
+++ tests/test_metrics_up.sh
@@ -4,13 +4,16 @@
 # 2. Ensures the Metrics endpoint returned a valid response
 # Depends on Curl
 
-AIRFLOW_SLEEP_DURATION=70 # Number of seconds to wait for airflow to start
+AIRFLOW_SLEEP_DURATION=90 # Number of seconds to wait for airflow to start
+ADMIN_ENDPOINT="http://localhost:8080/admin/"
 METRICS_ENDPOINT="http://localhost:8080/admin/metrics/"
 
 # Return nonzero status code if endpoint does not return 200
 CURL_FLAGS="--show-error --fail"
 
+echo "Waiting ${AIRFLOW_SLEEP_DURATION} seconds for Airflow to start before pinging"
 sleep ${AIRFLOW_SLEEP_DURATION}
+curl ${CURL_FLAGS} --silent ${ADMIN_ENDPOINT} && echo 'Admin console is up'
 curl ${CURL_FLAGS} ${METRICS_ENDPOINT}
 
 # TODO: validate the contents of the CURLed data
