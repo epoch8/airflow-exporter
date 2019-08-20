64a7fc05d58b9b1bd71678c2f5b8ec6fe3317bb8
Andrey Tatarinov <a@tatarinov.co>
2019-08-20 15:07:33 +0300

    local test script

--- run_test.sh
+++ run_test.sh
@@ -5,9 +5,11 @@ set -x
 
 export AIRFLOW_VERSION=1.10.4
 
-docker-compose down
-docker-compose up --build -d
-docker-compose exec airflow /entrypoint.sh airflow unpause dummy_dag
-docker-compose exec airflow /entrypoint.sh airflow trigger_dag dummy_dag
+docker-compose down -v --remove-orphans
 
-docker-compose -f docker-compose.yml -f docker-compose.test.yml run sut
+docker-compose up -d postgresql
+docker-compose run airflow /entrypoint.sh airflow initdb
+docker-compose run airflow /entrypoint.sh airflow unpause dummy_dag
+docker-compose run airflow /entrypoint.sh airflow trigger_dag dummy_dag
+
+docker-compose -f docker-compose.yml -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from=sut sut airflow
