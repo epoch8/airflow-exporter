1e501e8d6a34a071eb3c3065866475ff6627a8c9
Andrey Tatarinov <a@tatarinov.co>
2019-08-25 10:42:55 +0300

    add slow dag to tests

--- run_test.sh
+++ run_test.sh
@@ -10,6 +10,7 @@ docker-compose down -v --remove-orphans
 docker-compose up -d postgresql
 docker-compose run airflow /entrypoint.sh airflow initdb
 docker-compose run airflow /entrypoint.sh airflow unpause dummy_dag
+docker-compose run airflow /entrypoint.sh airflow unpause slow_dag
 docker-compose run airflow /entrypoint.sh airflow trigger_dag dummy_dag
 
 docker-compose -f docker-compose.yml -f docker-compose.test.yml up --abort-on-container-exit --exit-code-from=sut sut airflow
