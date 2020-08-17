c9db5202250b5f6fb9311c04b37996b768e64f3b
jingtao.yun <jyun@capsule.com>
2020-08-17 16:04:27 -0400

    Update prometheus_exporter.py
    Remove task_status metrics monitoring

--- airflow_exporter/prometheus_exporter.py
+++ airflow_exporter/prometheus_exporter.py
@@ -103,19 +103,19 @@ class MetricsCollector(object):
         # Task metrics
         # Each *MetricFamily generates two lines of comments in /metrics, try to minimize noise 
         # by creating new group for each dag
-        task_info = get_task_state_info()
-        for dag_id, tasks in itertools.groupby(task_info, lambda x: x.dag_id):
-            k, v = get_dag_labels(dag_id)
+#         task_info = get_task_state_info()
+#         for dag_id, tasks in itertools.groupby(task_info, lambda x: x.dag_id):
+#             k, v = get_dag_labels(dag_id)
 
-            t_state = GaugeMetricFamily(
-                'airflow_task_status',
-                'Shows the number of task starts with this status',
-                labels=['dag_id', 'task_id', 'owner', 'status', 'hostname'] + k
-            )
-            for task in tasks:
-                t_state.add_metric([task.dag_id, task.task_id, task.owners, task.state or 'none', task.hostname or 'none'] + v, task.value)
+#             t_state = GaugeMetricFamily(
+#                 'airflow_task_status',
+#                 'Shows the number of task starts with this status',
+#                 labels=['dag_id', 'task_id', 'owner', 'status', 'hostname'] + k
+#             )
+#             for task in tasks:
+#                 t_state.add_metric([task.dag_id, task.task_id, task.owners, task.state or 'none', task.hostname or 'none'] + v, task.value)
             
-            yield t_state
+#             yield t_state
 
         # Dag Metrics
         dag_info = get_dag_state_info()
