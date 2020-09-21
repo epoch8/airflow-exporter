4799009198858159082140ddfbe56cec3e8084ff
tonyyun <jyun@capsule.com>
2020-09-21 13:05:00 -0400

    flake8 linting issue fixes

--- tests/dags/dummy_dag.py
+++ tests/dags/dummy_dag.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.bash_operator import BashOperator
 
 
 default_args = {
