4cac0dccef9c174738ed377b6073844f6447d183
Andrey Tatarinov <a@tatarinov.co>
2021-02-10 13:26:28 +0300

    fix test

--- run_test.sh
+++ run_test.sh
@@ -6,6 +6,10 @@ set -x
 docker-compose down -v --remove-orphans
 
 docker-compose up -d postgres
+docker-compose up initdb
+
+docker-compose run scheduler scheduler -n 1
+
 docker-compose run scheduler dags unpause dummy_dag
 docker-compose run scheduler dags unpause slow_dag
 docker-compose run scheduler dags trigger dummy_dag
