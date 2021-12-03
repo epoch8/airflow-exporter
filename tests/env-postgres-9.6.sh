514eda9c6fc2635b881da6830e16b45b571fb6f3
Andrey Tatarinov <a@tatarinov.co>
2021-12-03 17:28:32 +0000

    try to setup multi-db CI

---
+++ tests/env-postgres-9.6.sh
@@ -0,0 +1,9 @@
+export RUN_DB=docker run -d \
+    --name db \
+    -p "5432:5432" \
+    --env POSTGRES_USER=airflow \
+    --env POSTGRES_PASSWORD=airflow \
+    --env POSTGRES_DB=airflow \
+    postgres:9.6
+
+export SQL_ALCHEMY_CONN=postgresql://airflow:airflow@localhost/airflow
