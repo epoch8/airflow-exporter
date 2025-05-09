84468a1a7e69e06e8c0fe2489e9aa6e432b8c783
Andrey Tatarinov <a@tatarinov.co>
2025-05-09 21:01:25 +0000

    update airflow version constraints to include 2.4.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     packages=["airflow_exporter"],
     setup_requires=["setuptools_scm"],
     install_requires=[
-        "apache-airflow>=2.0.0,<2.4",
+        "apache-airflow>=2.0.0,<2.5",
         "prometheus_client>=0.4.2",
     ],
     entry_points={
