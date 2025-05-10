757c7aa04bd55a118c747d161867d3f6256581bf
Andrey Tatarinov <a@tatarinov.co>
2025-05-10 09:28:51 +0000

    update airflow version constraint to allow apache-airflow<3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     packages=["airflow_exporter"],
     setup_requires=["setuptools_scm"],
     install_requires=[
-        "apache-airflow>=2.0.0,<2.11",
+        "apache-airflow>=2.0.0,<3",
         "prometheus_client>=0.4.2",
     ],
     entry_points={
