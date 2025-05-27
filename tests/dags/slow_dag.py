ff9c588ec0a73d256458c686f9a9564302316394
Swapnil Vaidya <vaidyaswapneel@gmail.com>
2025-05-27 16:44:35 +0530

    fix test for dummy and sow dag

--- tests/dags/slow_dag.py
+++ tests/dags/slow_dag.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.version import version as airflow_version
 from packaging.version import parse as parse_version
+import inspect
 
 if parse_version(airflow_version).major >= 3:
     from airflow.operators.empty import EmptyOperator as DummyOperator
@@ -26,18 +27,24 @@ default_args = {
     'max_active_runs': 1,
 }
 
-
-dag = DAG(
-    'slow_dag',
-    **{schedule_kw: timedelta(hours=5)},
-    default_args=default_args,
-    catchup=False,
-    params={
+dag_args = {
+    'dag_id': 'dummy_dag',
+    'schedule_interval': timedelta(hours=5),
+    'default_args': default_args,
+    'catchup': False,
+    'params': {
         'labels': {
             'kind': 'slow'
         }
     }
-)
+}
+
+sig = inspect.signature(DAG.__init__)
+
+if 'schedule_interval' not in sig.parameters:
+    dag_args['schedule'] = dag_args.pop('schedule_interval')
+
+dag = DAG(**dag_args)
 
 dummy1 = DummyOperator(
     task_id='dummy_task_1',
